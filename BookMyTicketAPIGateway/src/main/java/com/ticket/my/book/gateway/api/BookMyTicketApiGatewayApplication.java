081a4fa0cda8e665e3d76679ec959b0d677b80cf
raju.kabil@gmail.com <kabil.ariraman@d3banking.com>
2022-08-10 17:26:12 +0530

    added config server dependency in api-gateway

--- BookMyTicketAPIGateway/src/main/java/com/ticket/my/book/gateway/api/BookMyTicketApiGatewayApplication.java
+++ BookMyTicketAPIGateway/src/main/java/com/ticket/my/book/gateway/api/BookMyTicketApiGatewayApplication.java
@@ -2,11 +2,13 @@ package com.ticket.my.book.gateway.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableConfigServer
 public class BookMyTicketApiGatewayApplication {
 
 	public static void main(String[] args) {
